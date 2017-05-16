64347d5839f419fd62ef86fae89ed3d9890a2628
Robert Ventura <robert.ventura@trustyou.com>
2017-05-16 22:02:59 +0200

    Python compilats esborrats

---
+++ api/app.py
@@ -0,0 +1,53 @@
+import tornado.ioloop
+import tornado.web
+import tornado.httpclient
+import tornado.escape
+import motor
+import json
+
+class TeamsHandler(tornado.web.RequestHandler):
+   
+    def initialize(self, mongo_db):
+       self.mongo_db = mongo_db
+    
+    @tornado.gen.coroutine
+    def get(self):
+        cursor = mongo_db.teams.find({},{'_id': 0})
+        teams = []
+        while (yield cursor.fetch_next):
+            document = cursor.next_object()
+            teams.append(document)
+        result = {
+            'teams': teams
+        }
+        self.write(result)
+        
+class PlayersHandler(tornado.web.RequestHandler):
+   
+    def initialize(self, mongo_db):
+       self.mongo_db = mongo_db
+    
+    @tornado.gen.coroutine
+    def get(self, team):
+        cursor = mongo_db.players.find({'team':team},{'_id': 0})
+        players = []
+        while (yield cursor.fetch_next):
+            document = cursor.next_object()
+            players.append(document)
+        result = {
+            'players': players
+        }
+        self.write(result)
+
+def make_app(mongo_db):
+    return tornado.web.Application([
+        (r"/teams", TeamsHandler, dict(mongo_db=mongo_db)),
+        (r"/players/(.*)", PlayersHandler, dict(mongo_db=mongo_db)),
+    ])
+
+if __name__ == "__main__":
+    mongo_con = motor.motor_tornado.MotorClient('mongodb://admin:ventura2017@ds145780.mlab.com:45780/soccer_stats')
+    mongo_db = mongo_con.soccer_stats
+    app = make_app(mongo_db)
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
