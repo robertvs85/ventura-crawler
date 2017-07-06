679ebca8f892ef6d24bcc030824a8943d186a52e
xventura <xventura81@gmail.com>
2017-07-07 01:08:44 +0200

    - added single player query
    - added field to show when querying for all players in a team

--- api/app.py
+++ api/app.py
@@ -28,21 +28,33 @@ class PlayersHandler(tornado.web.RequestHandler):
        self.mongo_db = mongo_db
     
     @tornado.gen.coroutine
-    def get(self, team):
+    def get(self, team, field):
+        self.field_to_show = field
         cursor = mongo_db.players.find({'team':team},{'_id': 0})
         players = []
         while (yield cursor.fetch_next):
             document = cursor.next_object()
-            players.append(document)
+            players.append(document[field])
         result = {
             'players': players
         }
         self.write(result)
 
+class SinglePlayerHandler(tornado.web.RequestHandler):
+   
+    def initialize(self, mongo_db):
+       self.mongo_db = mongo_db
+    
+    @tornado.gen.coroutine
+    def get(self, player):
+        document = yield mongo_db.players.find_one({'nickname':player},{'_id': 0})
+        self.write(document)        
+        
 def make_app(mongo_db):
     return tornado.web.Application([
         (r"/teams", TeamsHandler, dict(mongo_db=mongo_db)),
-        (r"/players/(.*)", PlayersHandler, dict(mongo_db=mongo_db)),
+        (r"/players/(.*)/(.*)", PlayersHandler, dict(mongo_db=mongo_db)),
+        (r"/player/(.*)", SinglePlayerHandler, dict(mongo_db=mongo_db)),
     ])
 
 if __name__ == "__main__":
